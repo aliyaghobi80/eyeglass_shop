10e3cbdb3311a682bb4126ab8757fe2655650ec8
aliyaghobi <aliymix07@gmail.com>
2025-03-17 15:06:34 +0330

    upgrade endpoints and fix bug in back-end

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,36 @@
 class Constants {
-  static const String baseUrl = 'http://87.248.155.142:8000/api/';
-  static const String loginUrl = '${baseUrl}login/';
-  static const String registerUrl = '${baseUrl}signup/';
-  static const String productUrl = '${baseUrl}product/';
+  static const String baseUrl = 'http://87.248.155.142';
+
+  // Authentication endpoints
+  static const String loginUrl = '$baseUrl/api/login/';
+  static const String registerUrl = '$baseUrl/api/signup/';
+  static const String logoutUrl = '$baseUrl/api/logout/';
+  static const String refreshTokenUrl = '$baseUrl/api/token/refresh/';
+
+  // Product endpoints
+  static const String productUrl = '$baseUrl/api/product_list/';
+  static const String addProductUrl = '$baseUrl/api/add_product/';
+  static const String getProductUrl = '$baseUrl/api/product/';
+  static const String deleteProductUrl = '$baseUrl/api/delete_product/';
+  static const String editProductUrl = '$baseUrl/api/edit_product/';
+
+  // Category endpoints
+  static const String categoriesUrl = '$baseUrl/api/categories/';
+  static const String addCategoryUrl = '$baseUrl/api/add_category/';
+  static const String editCategoryUrl = '$baseUrl/api/edit_category/';
+  static const String deleteCategoryUrl = '$baseUrl/api/delete_category/';
+
+  // Helper method for getting product-specific URLs
+  static String getProductDetailUrl(int productId) =>
+      '$getProductUrl$productId/';
+  static String getDeleteProductUrl(int productId) =>
+      '$deleteProductUrl$productId/';
+  static String getEditProductUrl(int productId) =>
+      '$editProductUrl$productId/';
+
+  // Helper method for getting category-specific URLs
+  static String getEditCategoryUrl(int categoryId) =>
+      '$editCategoryUrl$categoryId/';
+  static String getDeleteCategoryUrl(int categoryId) =>
+      '$deleteCategoryUrl$categoryId/';
 }
